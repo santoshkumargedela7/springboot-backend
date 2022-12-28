197dc2a500c017e93b7332ac58bb0050f7d7dd00
santoshkumargedela7 <santoshkumargedela7@gmail.com>
2022-12-28 16:21:41 +0530

    fourthcommit

---
+++ springboot-backend/springboot-backend/src/main/java/net/javaguides/springboot/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+		
+	@Bean
+	public Docket getDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("public-apis")
+				.apiInfo(getApiInfo()).select()
+				//.apis(RequestHandlerSelectors.basePackage("net.javaguides.springboot.controller"))
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.build();
+	}
+	
+	public ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("siva code apis")
+				.description("Api's created by savecode").version("1").build();
+	}
+}
